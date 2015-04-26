fc32fcd4defd56b688f878b55ada7d14d0dd7dc0
unknown <versan.mihai@gmail.com>
2015-04-26 15:02:20 +0300

    commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tema2_2.Models;
+
+namespace Tema2_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private CartController cc = new CartController();
+        //Cart c = new Cart();
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        
+
+        //public ActionRezult Buy()
+        public ActionResult Buy()
+        {
+            //ViewBag.Message = cc.ShowCart();
+            ViewBag.Message = cc.ShowCart();
+            return View(); 
+            //return cc.ShowCart();
+        }
+
+        public void AddToCart(int? id)
+        {
+            String prevPage = Request.UrlReferrer.ToString();
+            if (id == null)
+            {
+                throw new NullReferenceException();
+            }
+            Product product = db.Products.Find(id);
+           // Console.Write("Adaugare!\n");
+            if (product != null && product.Quantity>0)
+            {
+                cc.Add(product);
+                
+            }
+            //return this.Index();
+            //Index();
+            Response.Redirect(prevPage);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        [Authorize(Roles = "canEdit")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "canEdit")]
+        public ActionResult Create([Bind(Include = "ID,Name,Quantity,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        [Authorize(Roles = "canEdit")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "canEdit")]
+        public ActionResult Edit([Bind(Include = "ID,Name,Quantity,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        [Authorize(Roles = "canEdit")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "canEdit")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/Cart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tema2_2.Models
+{
+    public class Cart
+    {
+        public Product[] products { get; set; }
+        public double TotalPrice { get; set; }
+        public int Len = 0;
+
+        public Cart()
+        {
+            products = new Product[100];
+        }
+
+        public void AddToCart(Product p)
+        {
+            //Console.WriteLine("-----Adaugare in cart!--------");
+            products[Len] = new Product();
+            products[Len] = p;
+            Len++;
+        }
+
+        public double CalculateTotalCost()
+        {
+            double cost = 0;
+            for(int i=0; i<Len; i++)
+            {
+                cost += products[i].Price;
+            }
+            return cost;
+        }
+
+        public String CartProducts()
+        {
+            //Console.WriteLine("-----Afisare PRODUSE CART!!!--------");
+            if(products == null)
+            {
+                return "Cart is empty!";
+            }
+
+            String s = "Products:\n";
+            
+            for(int i=0; i<Len; i++)
+            {
+                s = s + products[i].Name + products[i].Price + "\n";
+            }
+            return s +";Total cost: "+ CalculateTotalCost();
+        }
+    }
+}
